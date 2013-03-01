53c93202d6b9a0c70d910d504cc008bd2d511f78
rohan1020 <rohanraja9@gmail.com>
2013-03-01 22:01:53 +0530

    better

--- js/yql.js
+++ js/yql.js
@@ -1,7 +1,20 @@
 function searchquery(squery)
 {
+	srchurl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20boss.search%20where%20q%3D%22";
+	
+	srchurl = srchurl + squery;
+		
+	srchurl = srchurl + "%22%20and%20secret%20%3D%20%22a3d93853ba3bad8a99a175e8ffa90a702cd08cfa%22%20and%20ck%3D%22dj0yJmk9YWF3ODdGNWZPYjg2JmQ9WVdrOWVsWlZNRk5KTldFbWNHbzlNVEEyTURFNU1qWXkmcz1jb25zdW1lcnNlY3JldCZ4PTUz%22%20AND%20sites%20%3D%20%22lyrics007.com%22%3B&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys" ;
+	
+	
+	srchurl = srchurl.replace(/\ /g,"%20");
+	
+	console.log(srchurl);
+	
+	//alert(srchurl);
+	
 	$.ajax({
-		url: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20boss.search%20where%20q%3D%22I%20got%20my%20first%20real%20six%20string%22%20and%20secret%20%3D%20%22a3d93853ba3bad8a99a175e8ffa90a702cd08cfa%22%20and%20ck%3D%22dj0yJmk9YWF3ODdGNWZPYjg2JmQ9WVdrOWVsWlZNRk5KTldFbWNHbzlNVEEyTURFNU1qWXkmcz1jb25zdW1lcnNlY3JldCZ4PTUz%22%20AND%20sites%20%3D%20%22lyrics007.com%22%3B&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+		url: srchurl,
 		type: "GET",
 		dataType: 'json',
 		async: true,
@@ -9,8 +22,9 @@ function searchquery(squery)
 		
 		//$('body').html(data.query.count);
 		console.log(data);
-		console.log(data.query.results.bossresponse.web.results.result);
+		//console.log(data.query.results.bossresponse.web.results.result);
 		
+		document.getElementById("searchresults").innerHTML = "";
 		
 		$.each(data.query.results.bossresponse.web.results.result, function(index, value) {
 		
@@ -20,7 +34,7 @@ function searchquery(squery)
 		 	outp += value.title.content ;
 		 	outp += "</a><br>"
 		 	
-		 	document.body.innerHTML += outp;
+		 	document.getElementById("searchresults").innerHTML += outp;
 		 
 		});
 
